29aee913830c69652d57fd2ccd722a906db3a38e
MBorecki <borecki2@gmail.com>
2015-12-22 21:52:38 +0100

    config file

--- app/app.js
+++ app/app.js
@@ -2,17 +2,21 @@ var express = require('express');
 var apiRouter = require('./api/router.js');
 var pageRouter = require('./page/router.js');
 
+var CONFIG = require('./config.json');
+
 var app = express();
 var server = null;
 
-var PORT = 8080;
+var PORT = CONFIG.SERVER_PORT;
 
 app.use(function(req, res, next){
-console.log('REQUEST: [' + req.method + '] ' + req.path);
-next();
-})
+    console.log('REQUEST: [' + req.method + '] ' + req.path);
+    next();
+});
 
 app.use('/api', apiRouter);
 app.use(pageRouter);
 
-app.listen(PORT);
+app.listen(PORT, function(){
+    console.log('Server started on: ' + PORT);
+});
